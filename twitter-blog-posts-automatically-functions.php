4803a1285e1db7198bd0e369bd712a05221cf851
ss88 <ss88@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-05 06:23:55 +0000

    * added file_get_contents if curl is not working * added new post format drop down. Setting this to 'status' is your template supports it is best for Twitter posts. * Fixed some HTML issues in the admin area
    git-svn-id: https://plugins.svn.wordpress.org/twitter-blog-posts-automatically/tags/1.2@608206 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ twitter-blog-posts-automatically-functions.php
@@ -0,0 +1,111 @@
+<?php
+	// Add it to the menu
+	function t2wp_admin_actions() {
+		add_options_page('Twitter to Blog Posts', 'Twitter to Blog Posts', 1, 't2wp-admin', 't2wp_admin');
+	};	
+
+	// Display the Admin
+	function t2wp_admin() {
+		include('t2wp-admin.php');
+	} 
+
+	// Activate the Hourly Update
+	function t2wp_activation() {
+		wp_schedule_event(current_time('timestamp'), 'hourly', 't2wp_hourly_update_action');
+		update_option('t2wp_lastran', current_time('timestamp', 1));
+	}
+
+	// Perform the hourly update
+	function t2wp_hourly_update() {
+		t2wp_search();
+	}
+
+	// Deactivate the Hourly Update
+	function t2wp_deactivation() {
+		wp_clear_scheduled_hook('t2wp_hourly_update_action');
+	}
+	
+	// Curl or file_get_contents
+	function t2wp_curl($query) {
+	
+		if (!function_exists('curl_init')){ 
+			$ch = file_get_contents('http://search.twitter.com/search.json?rpp=100&q=' . urlencode($query));
+			$content = trim(curl_exec($ch));
+		}
+		else
+		{
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, 'http://search.twitter.com/search.json?rpp=100&q=' . urlencode($query));  
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_TIMEOUT, '5');
+			$content = trim(curl_exec($ch));
+			curl_close($ch);
+		}
+		
+		return $content;
+	}
+	
+	// Search Twitter API
+	function t2wp_search($returnTotal = FALSE) {
+	
+		// Get the values
+		$hashtags = get_option('t2wp_hashtags');
+		$usernames = get_option('t2wp_usernames');
+		$category = get_option('t2wp_category');
+		$user = get_option('t2wp_user');
+		$format = get_option('t2wp_format');
+		
+		// Run Hashtags
+		if(!empty($hashtags))
+		{
+			$hashes = explode(',', $hashtags);
+			foreach($hashes as $hash)
+				$query = $hash . '+OR+';
+			
+			$result = json_decode(t2wp_curl($query), TRUE);
+			$totalInserted += t2wp_insert($result['results']);
+		}
+		
+		// Run Usernames
+		if(!empty($usernames))
+		{
+			$users = explode(',', $usernames);
+			foreach($users as $user)
+				$query = 'from:'.$user . '+OR+';
+			
+			$result = json_decode(t2wp_curl($query), TRUE);
+			$totalInserted += t2wp_insert($result['results']);
+		}
+		
+		if($returnTotal==TRUE)
+			return $totalInserted;
+	}
+	
+	// Insert a Post
+	function t2wp_insert($array) {
+	
+		$lastRan = get_option('t2wp_lastran');
+	
+		foreach($array as $object)
+		{		
+			if(strtotime($object['created_at']) > $lastRan)
+			{
+				$StatusID = $object['id_str'];
+                $post['post_date'] = date("Y-m-d H:i:s",strtotime($object['created_at']));
+                $post['post_content'] = 'https://twitter.com/'. $object['from_user'] .'/status/' . $StatusID;
+				$post['post_status'] = 'publish';
+				$post['post_author'] = get_option('t2wp_user');
+				$post['post_category'] = array(get_option('t2wp_category'));
+				$post['post_title'] = 'Tweeted: ' . $post['post_date'];
+				$post['post_slug'] = 'Tweeted: ' . $post['post_date'];
+				$post['post_type'] = $format;
+				
+                @wp_insert_post($post);
+				update_option('t2wp_lastran', current_time('timestamp', 1));
+				$totalInserted++;
+			}
+		}
+	
+		return $totalInserted;
+	}
+?>
