7886e1cc0484f361dc154e06bb50fd165623685e
ss88 <ss88@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-09 18:53:14 +0000

    changed version
    git-svn-id: https://plugins.svn.wordpress.org/twitter-blog-posts-automatically/trunk@623136 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- twitter-blog-posts-automatically.php
+++ twitter-blog-posts-automatically.php
@@ -3,7 +3,7 @@
    Plugin Name: Twitter Blog Posts Automatically
    Plugin URI: http://www.ss88.co.uk/twitter-blog-posts-automatically/
    Description: Publish your Tweets to your WordPress blog automatically.
-   Version: 1.2
+   Version: 1.3
    Author: Steven Sullivan
    Author URI: http://www.ss88.co.uk/
    License: GPL2
